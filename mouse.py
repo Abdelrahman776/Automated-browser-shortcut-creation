d38832d5022c63a2e708c6b4f50a222e3bda9708
Abdelrahman kamal <abdelrahmannkamal@gmail.com>
2023-06-10 15:10:34 +0300

    modified

--- mouse.py
+++ mouse.py
@@ -3,7 +3,7 @@ import pyautogui
 pyautogui.click(1890,70,duration=0.01) # moving to the drop down menu on top right of brave browser then clicking
 # pyautogui.click()
 pyautogui.moveRel(0,677,duration=0.01)# moving to more tools option then click
-pyautogui.click()
+pyautogui.click() 
 
 pyautogui.moveRel(-520,30)  # from more tools choose "create shortcut" option
 pyautogui.click()
